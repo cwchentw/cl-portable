b1e521e82d95419c2cf670ca9bac4f601f9af0d2
Michelle Chen <cwchen.tw@outlook.com>
2022-03-11 22:27:20 +0800

    Update some example code

--- examples/helloworld-cjk.lisp
+++ examples/helloworld-cjk.lisp
@@ -1,5 +1,7 @@
 ;; Load cl-portable through Quicklisp silently.
-(ql:quickload "cl-portable" :silent t)
+#+quicklisp (ql:quickload "cl-portable" :silent t)
+;; Alternatively, load a local cl-portable.lisp script.
+#-quicklisp (load "cl-portable.lisp" :print nil)
 
 (use-package 'cl-portable)
 
